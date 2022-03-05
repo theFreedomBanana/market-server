263600463226bb32873828b74e10893bf70c3748
Ben <benjamin.mahmoudi@gmail.com>
2022-03-05 13:49:40 +0100

    add tags and company count filters

--- index.js
+++ index.js
@@ -1,14 +1,45 @@
-const jsonServer = require("json-server")
-const server = jsonServer.create()
-const router = jsonServer.router("db.json")
+const fs = require('fs');
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
 
-const middlewares = jsonServer.defaults()
+const items = JSON.parse(fs.readFileSync("db.json", "utf8")).items;
 
-server.use(middlewares)
-server.use(router)
+countPerCompany = {};
+const countPerTag = {};
+items.forEach(({ manufacturer, tags }) => {
+	if (countPerCompany[manufacturer]) {
+		countPerCompany[manufacturer]++;
+	} else {
+		countPerCompany[manufacturer] = 1;
+	}
 
-const port = process.env.PORT || 3000
+	tags.forEach((tag) => {
+		if (countPerTag[tag]) {
+			countPerTag[tag]++;
+		} else {
+			countPerTag[tag] = 1;
+		}
+	});
+});
 
+server.use(middlewares);
+
+const port = process.env.PORT || 3000;
+
+server.get(
+	"/counts/:filter/",
+	(req, res) => {
+		if (req.params.filter === "company") {
+			res.jsonp(countPerCompany);
+		} else if (req.params.filter === "tags") {
+			res.jsonp(countPerTag);
+		}
+	},
+);
+
+server.use(router);
 server.listen(port, () => {
 	console.log(`Market DB is running on port ${port}`)
 })
